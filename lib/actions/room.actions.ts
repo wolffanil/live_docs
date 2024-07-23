0c7f0567f614da0cdf5da7312fa026173f9acc46
wolffanil2 <wolffanil@mail.ru>
2024-07-23 18:44:36 +0400

    fix5

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,10 +1,10 @@
 "use server";
 
 import { nanoid } from "nanoid";
+import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
 import { getAccessType, parseStringify } from "../utils";
 import { redirect } from "next/navigation";
-import { liveblocks } from "../liveblocks";
 
 export const createDocument = async ({
   userId,

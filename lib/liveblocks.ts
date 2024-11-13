61796ab4f4de56433dd4aae94a0f8c129bf24024
wolffanil2 <wolffanil@mail.ru>
2024-11-14 01:25:09 +0400

    fix 5

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -1,13 +1,5 @@
-<<<<<<< HEAD:lib/liveblocks.ts
 import { Liveblocks } from "@liveblocks/node";
 
 export const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY as string,
 });
-=======
-import { Liveblocks } from "@liveblocks/node";
-
-export const liveblocks = new Liveblocks({
-  secret: process.env.LIVEBLOCKS_SECRET_KEY as string | string,
-});
->>>>>>> main:lib/liveBlocks.ts
